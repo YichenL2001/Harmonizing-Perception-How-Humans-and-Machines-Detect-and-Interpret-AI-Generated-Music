38ced1adc906e97c45488dc58a71586c2e1b0c46
Yi ChenLiu <67210088+YichenL2001@users.noreply.github.com>
2026-01-09 18:58:00 -0500

    Delete vit_spectttra_convnext directory

--- vit_spectttra_convnext/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from sonics.layers.tokenizer import Tokenizer1D, STTokenizer
-from sonics.layers.embedding import (
-    SinusoidPositionalEncoding,
-    LearnedPositionalEncoding,
-)
-from sonics.layers.transformer import Transformer
